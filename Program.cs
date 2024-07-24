03ddd70cb848cd7ba300660766e9343c73ac3630
ThanhDuc2111 <117926355+ThanhDuc2111@users.noreply.github.com>
2024-07-24 10:11:35 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTTL
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] array = { 34, 7, 23, 32, 5, 62 };
+            QuickSort(array, 0, array.Length - 1);
+
+            Console.WriteLine("Sorted array:");
+            foreach (int item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.ReadKey();
+
+        }
+
+        static void QuickSort(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivotIndex = Partition(array, left, right);
+                QuickSort(array, left, pivotIndex - 1);
+                QuickSort(array, pivotIndex + 1, right);
+            }
+        }
+
+        static int Partition(int[] array, int left, int right)
+        {
+            int pivot = array[right];
+            int i = left - 1;
+
+            for (int j = left; j < right; j++)
+            {
+                if (array[j] <= pivot)
+                {
+                    i++;
+                    Swap(array, i, j);
+                }
+            }
+            Swap(array, i + 1, right);
+            return i + 1;
+        }
+
+        static void Swap(int[] array, int a, int b)
+        {
+            int temp = array[a];
+            array[a] = array[b];
+            array[b] = temp;
+        }
+    }
+}
